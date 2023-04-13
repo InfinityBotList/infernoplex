1f4930d025bca115ef2777c77c47b1df66c8d7d4
Your Name <cheesycod@outlook.com>
2023-04-13 13:01:45 +0530

    initial structure

---
+++ src/config.rs
@@ -0,0 +1,84 @@
+use once_cell::sync::Lazy;
+use poise::serenity_prelude::GuildId;
+use serde::{Deserialize, Serialize};
+use std::{fs::File, io::Write, num::NonZeroU64};
+
+use crate::Error;
+
+/// Global config object
+pub static CONFIG: Lazy<Config> = Lazy::new(|| Config::load().expect("Failed to load config"));
+
+#[derive(Serialize, Deserialize)]
+pub struct Servers {
+    pub main: GuildId,
+    pub staff: GuildId,
+}
+
+impl Default for Servers {
+    fn default() -> Self {
+        Self {
+            main: GuildId(NonZeroU64::new(758641373074423808).unwrap()),
+            staff: GuildId(NonZeroU64::new(870950609291972618).unwrap()),
+        }
+    }
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct Config {
+    pub database_url: String,
+    pub client_secret: String,
+    pub token: String,
+    pub servers: Servers,
+    pub frontend_url: String,
+    pub proxy_url: String,
+    pub infernoplex_domain: String,
+}
+
+impl Default for Config {
+    fn default() -> Self {
+        Self {
+            database_url: String::from(""),
+            token: String::from(""),
+            client_secret: String::from(""),
+            servers: Servers::default(),
+            frontend_url: String::from("https://infinitybots.gg"),
+            proxy_url: String::from("http://127.0.0.1:3219"),
+            infernoplex_domain: String::from("https://infernoplex.infinitybots.gg"),
+        }
+    }
+}
+
+impl Config {
+    pub fn load() -> Result<Self, Error> {
+        // Delete config.yaml.sample if it exists
+        if std::path::Path::new("config.yaml.sample").exists() {
+            std::fs::remove_file("config.yaml.sample")?;
+        }
+
+        // Create config.yaml.sample
+        let mut sample = File::create("config.yaml.sample")?;
+
+        // Write default config to config.yaml.sample
+        sample.write_all(serde_yaml::to_string(&Config::default())?.as_bytes())?;
+
+        // Open config.yaml
+        let file = File::open("config.yaml");
+
+        match file {
+            Ok(file) => {
+                // Parse config.yaml
+                let cfg: Config = serde_yaml::from_reader(file)?;
+
+                // Return config
+                Ok(cfg)
+            }
+            Err(e) => {
+                // Print error
+                println!("config.yaml could not be loaded: {}", e);
+
+                // Exit
+                std::process::exit(1);
+            }
+        }
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,184 @@
+use std::{time::Duration, num::NonZeroU64};
+
+use log::{info, error};
+use poise::serenity_prelude::{FullEvent, GuildId};
+use sqlx::{postgres::PgPoolOptions, PgPool};
+
+use crate::cache::CacheHttpImpl;
+
+mod config;
+mod checks;
+mod help;
+mod crypto;
+mod cache;
+mod stats;
+
+type Error = Box<dyn std::error::Error + Send + Sync>;
+type Context<'a> = poise::Context<'a, Data, Error>;
+
+// User data, which is stored and accessible in all command invocations
+pub struct Data {
+    pool: sqlx::PgPool,
+    cache_http: cache::CacheHttpImpl,
+}
+
+#[poise::command(prefix_command)]
+async fn register(ctx: Context<'_>) -> Result<(), Error> {
+    poise::builtins::register_application_commands_buttons(ctx).await?;
+    Ok(())
+}
+
+async fn on_error(error: poise::FrameworkError<'_, Data, Error>) {
+    // This is our custom error handler
+    // They are many errors that can occur, so we only handle the ones we want to customize
+    // and forward the rest to the default handler
+    match error {
+        poise::FrameworkError::Setup { error, .. } => panic!("Failed to start bot: {:?}", error),
+        poise::FrameworkError::Command { error, ctx } => {
+            error!("Error in command `{}`: {:?}", ctx.command().name, error,);
+            let err = ctx
+                .say(format!(
+                    "There was an error running this command: {}",
+                    error
+                ))
+                .await;
+
+            if let Err(e) = err {
+                error!("SQLX Error: {}", e);
+            }
+        }
+        poise::FrameworkError::CommandCheckFailed { error, ctx } => {
+            error!(
+                "[Possible] error in command `{}`: {:?}",
+                ctx.command().name,
+                error,
+            );
+            if let Some(error) = error {
+                error!("Error in command `{}`: {:?}", ctx.command().name, error,);
+                let err = ctx
+                    .say(format!(
+                        "**{}**",
+                        error
+                    ))
+                    .await;
+
+                if let Err(e) = err {
+                    error!("Error while sending error message: {}", e);
+                }
+            }
+        }
+        error => {
+            if let Err(e) = poise::builtins::on_error(error).await {
+                error!("Error while handling error: {}", e);
+            }
+        }
+    }
+}
+
+async fn event_listener(event: &FullEvent, user_data: &Data) -> Result<(), Error> {
+    match event {
+        FullEvent::InteractionCreate {
+            interaction,
+            ctx: _,
+        } => {
+            info!("Interaction received: {:?}", interaction.id());
+        },
+        FullEvent::Ready {
+            data_about_bot,
+            ctx: _,
+        } => {
+            info!(
+                "{} is ready!",
+                data_about_bot.user.name
+            );
+        },
+        _ => {}
+    }
+
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() {
+    const MAX_CONNECTIONS: u32 = 3; // max connections to the database, we don't need too many here
+
+    std::env::set_var("RUST_LOG", "infernoplex=info");
+
+    env_logger::init();
+
+    info!("Proxy URL: {}", config::CONFIG.proxy_url);
+
+    let http = serenity::all::HttpBuilder::new(&config::CONFIG.token)
+        .proxy(config::CONFIG.proxy_url.clone())
+        .ratelimiter_disabled(true)
+        .build();
+
+    let client_builder =
+        serenity::all::ClientBuilder::new_with_http(http, serenity::all::GatewayIntents::all());
+
+    let framework = poise::Framework::new(
+        poise::FrameworkOptions {
+            initialize_owners: true,
+            prefix_options: poise::PrefixFrameworkOptions {
+                prefix: Some("sl!".into()),
+                ..poise::PrefixFrameworkOptions::default()
+            },
+            listener: |event, _ctx, user_data| Box::pin(event_listener(event, user_data)),
+            commands: vec![
+                register(),
+                help::help(),
+                help::simplehelp(),
+                stats::stats(),
+            ],
+            /// This code is run before every command
+            pre_command: |ctx| {
+                Box::pin(async move {
+                    info!(
+                        "Executing command {} for user {} ({})...",
+                        ctx.command().qualified_name,
+                        ctx.author().name,
+                        ctx.author().id
+                    );
+                })
+            },
+            /// This code is run after every command returns Ok
+            post_command: |ctx| {
+                Box::pin(async move {
+                    info!(
+                        "Done executing command {} for user {} ({})...",
+                        ctx.command().qualified_name,
+                        ctx.author().name,
+                        ctx.author().id
+                    );
+                })
+            },
+            on_error: |error| Box::pin(on_error(error)),
+            ..Default::default()
+        },
+        move |ctx, _ready, _framework| {
+            Box::pin(async move {
+                Ok(Data {
+                    cache_http: CacheHttpImpl {
+                        cache: ctx.cache.clone(),
+                        http: ctx.http.clone(),
+                    },
+                    pool: PgPoolOptions::new()
+                        .max_connections(MAX_CONNECTIONS)
+                        .connect(&config::CONFIG.database_url)
+                        .await
+                        .expect("Could not initialize connection"),
+                    
+                })
+            })
+        },
+    );
+
+    let mut client = client_builder
+        .framework(framework)
+        .await
+        .expect("Error creating client");
+
+    if let Err(why) = client.start().await {
+        error!("Client error: {:?}", why);
+    }
+}
